adb8e79b8fc038fae9ec4858306a92a165e52ae6
whetherit <glebagafov434@gmail.com>
2023-01-15 01:33:08 +0800

    add new files

---
+++ untitled/main.cpp
@@ -0,0 +1,40 @@
+#include "iostream"
+#include <string>
+int main(){
+    long long a, b, c;
+    char d;
+   std::cout << "(support: +, -, *, /)" << std::endl << "Write 1 nunmber >> ";
+   std::cin >>a;
+   std::cout << "Write 2 nunmber> >";
+   std::cin >>b;
+   std::cout << "Choice >>";
+   std::cin >> d;
+   if (d == '+')
+   {
+        c = a + b;
+        std::cout << c;
+    }
+   if (d == '-')
+   {
+       c = a - b;
+       std::cout << c;
+   }
+    if (d == '*')
+    {
+        c = a * b;
+        std::cout << c;
+    }
+    if (d == '/')
+    {
+        c = a / b;
+        std::cout << c;
+    }
+}
+
+
+
+
+
+
+
+
